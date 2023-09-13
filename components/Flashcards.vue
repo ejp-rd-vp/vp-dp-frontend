4dc0cb201d05a52021b5559d294b9ebc7dcbd6a1
Larie <larie.borelle.siabou@ges.thm.de>
2023-09-13 17:25:44 +0200

    #76-Add flashcards
    - Add flashcards content

--- components/Flashcards.vue
+++ components/Flashcards.vue
@@ -4,18 +4,28 @@ export default {
   data () {
     return {
       colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
+        'purple lighten-2',
+        'yellow darken-3',
+        'blue ',
+        'pink lighten-1',
+
+        'lime darken-3',
         'red lighten-1',
-        'deep-purple accent-4',
+        'green darken-1',
+        'deep-orange darken-1',
+
+
+
       ],
       slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth',
+        'As a clinical researcher I would like to\tknow if there is already data about my patient in GPAP\tso that\tI can check whether their data is already submitted to GPAP\n',
+        'As a clinical researcher I would like to\tknow if there is already data about my patient in GPAP\tso that\tI can study the phenotype information associated with the genome of my patient\n',
+        'As a data scientist I would like to\ttrain AI models on combined datasets from GPAP and patient registries\tso that\tI can discover the mean age of patients with a specific profile\n',
+        'As a data steward I would like to\toffer discovery of data use conditions about my patient registry data\tso that\tmore researchers know how they can use this data in their work\n',
+        'As a clinical researcher I would like to\tsee or filter on data use conditions\tso that\tI can quickly identify data sources I am allowed to use in my research\n',
+        'As a data steward I would like to\tgenerate a FAIRification plan\tso that\tadvise colleagues building the system architecture\n',
+        'As a data scientist I would like to\tdevelop a computational workflow that can be used for multiple types of data sets\tso that\tI can design an umbrella study that covers multiple types of diseases\n',
+        'As a clinical researcher I would like to\tknow the addresses of all hospitals that see patients with LGMD in Italy\tso that\tI can contact them with the latest standards of care\n'
       ],
     }
   },
@@ -42,8 +52,10 @@ export default {
           align="center"
           justify="center"
         >
-          <div class="text-h2">
-            {{ slide }} Slide
+          <div class="flashcards-h2">
+            <div>
+            {{ slide }}
+            </div>
           </div>
         </v-row>
       </v-sheet>
@@ -53,5 +65,14 @@ export default {
 
 
 <style scoped lang="scss">
+
+.flashcards-h2 {
+  font-size: 50px;
+  text-align: center;
+  color: white;
+  padding: 2rem;
+  margin: 0 1rem;
+
+}
 
 </style>
