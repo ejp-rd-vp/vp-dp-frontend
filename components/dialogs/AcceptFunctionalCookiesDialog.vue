71437554da050969d9dc4b22136213ffd0631266
Larie Borelle Siabou <larie.borelle.siabou@ges.thm.de>
2024-04-03 09:11:51 +0200

    96-cookie-consent
    Adjust information about cookies

--- components/dialogs/AcceptFunctionalCookiesDialog.vue
+++ components/dialogs/AcceptFunctionalCookiesDialog.vue
@@ -4,11 +4,6 @@ export default {
     return {
       dialog: true,
       usedCookies: [
-        'auth._refresh_token.keycloak',
-        'auth._refresh_token_expiration.keycloak',
-        'auth._token.keycloak',
-        'auth._token_expiration.keycloak',
-        'auth.keycloak.pkce_state',
         'auth.strategy',
         'cookiesAccepted'
       ]
